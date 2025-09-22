5427a3208e73a595f83d9d0b955ed8c092fa86e6
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-09-22 18:57:46 -0300

    corrigido telefone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -62,7 +62,7 @@ const Footer = () => {
                                 <Phone className="text-secondary mt-1" size={18} />
                                 <div>
                                     <p className="font-medium">WhatsApp</p>
-                                    <p className="text-primary-foreground/80">(19) 98216-3039</p>
+                                    <p className="text-primary-foreground/80">(19) 99534-9375</p>
                                 </div>
                             </div>
                             <div className="flex items-start space-x-3">
